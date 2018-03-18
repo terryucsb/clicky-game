ac0e98f1f3ec28eecdad52a1314f62fe84b52409
Terry Kim <terryucsb@gmail.com>
2018-03-18 01:34:20 -0700

    Add files via upload
    export from local directory

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# clicky-game
+# Clicky-Game #
+Clicky-game is a memory game developed with React. This assignment required me to break up my application's UI into components, manage component state, and respond to
+
+<img src="https://vignette.wikia.nocookie.net/pocketmonsters/images/6/64/Ash_%26_Friends_Group_OS_Anime.png/revision/latest?cb=20130928200233" height="150x" width="200px"/>
+
+#Instructions#
+1 - Check out the example solution and study the app's basic functionality.
+
+2 - Create a new React application using Create React App.
+
+3 - The application should render different images (of your choice) to the screen. Each image should listen for click events.
+
+4 - The application should keep track of the user's score. The user's score should be incremented when clicking an image for the first time. The user's score should be reset to 0 if they click the same image more than once.
+
+5 - Every time an image is clicked, the images rendered to the page should shuffle themselves in a random order.
+
+6 - Once the user's score is reset after an incorrect guess, the game should restart.
+
+7 - When complete, the application should be deployed to Github Pages. See the README generated with Create React App for instructions on deploying the application to Github Pages.
+  
+
+[Week 14 Homework - Terry Kim]
---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
+
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on the picture but not more than once";
+
+class App extends Component {
+
+    state = {
+        matches: matches,
+        correctGuesses: 0,
+        bestScore: 0,
+        clickMessage: ""
+    };
+
+    setClicked = id => {
+
+        const matches = this.state.matches;
+
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "AWWW you already chose that one, try again."
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        } else if (correctGuesses < 11) {
+
+            clickedMatch[0].clicked = true;
+
+          
+            correctGuesses++;
+            
+            clickMessage = "Good Memory Keep Going!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            clickedMatch[0].clicked = true;
+
+            correctGuesses = 0;
+
+            clickMessage = "Fantastic, You Are Elbert Einstein!";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>To boldly click where no one has clicked before!</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
